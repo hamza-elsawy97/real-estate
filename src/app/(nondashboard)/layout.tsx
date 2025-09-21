cd76fcb3fe546959c7cf506df000f14624dbe066
hamza-elsawy97 <153780196+hamza-elsawy97@users.noreply.github.com>
2025-09-21 22:17:08 +0300

    upload all basic files with navbar missed style

---
+++ src/app/(nondashboard)/layout.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '@/components/Navbar'
+
+const layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div>
+    <Navbar />
+      <main className="h-full w-full flex flex-col pt-[${NAVBAR_HEIGHT}px]">
+        {children}
+      </main>
+    </div>
+  )
+}
+
+export default layout
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,25 @@
+import { NAVBAR_HEIGHT } from '@/lib/constants';
+import React from 'react';
+import Link from 'next/link';
+
+const Navbar = () => {
+  return (
+    <div className='fixed top-0 left-0 w-fill z-50 shadow-xl'
+    style={{height: `${NAVBAR_HEIGHT}px`}}>
+      <div className='flex justify-between item-center w-full py-3 px-8 bg-primary-700 text-white'>
+        <div className="flex item-center gap-4 md:gap-6">
+          <Link href="/" className="cursor-pointer hover:!text-primary-300" scroll={false}>
+            <div className="flex items-center gap-3">
+              <img src="/logo.svg" alt="logo" className="w-6 h-6" />
+              <div className="font-bold text-xl">NEXT.JS
+                <span className="text-secondary-500 font-light hover:!text-primary-300">Template</span>
+              </div>
+            </div>
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
